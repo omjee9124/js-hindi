6ad86ecdd8b4e3b09e7e9495e1f44d8511b0a246
OM JI DUBEY <omjeed0666@gmail.com>
2025-04-26 09:56:39 +0000

    Conversions Of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "om"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+33 -> 33
+"33abc" -> NaN
+"abc" -> NaN
+null -> 0
+true/false ->1/0
+undefined -> NaN
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+/*
+1-> true
+0-> false
+""-> false
+"om" -> true
+*/
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
