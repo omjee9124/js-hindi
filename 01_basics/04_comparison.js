64480d1954f747e5fccc123e0a5c744e4e71956b
OM JI DUBEY <omjeed0666@gmail.com>
2025-04-26 11:01:01 +0000

    Comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+/*
+In javascript the equality check (==) and comparison operator works differently because comparison converts null to a number whether == it simply check the equality
+*/
