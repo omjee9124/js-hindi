773f9cbe46784841902e4789944042e244dd6f35
OM JI DUBEY <omjeed0666@gmail.com>
2025-05-30 14:33:13 +0000

    date and time

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,33 @@
+const myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toLocaleString())
+
+// let myCreatedDate = new Date("2023-01-15")//YYYY-MM-DD
+let myCreatedDate = new Date("09-22-2025") //,MM-DD-YYYY
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+
+// console.log(Math.floor(Date.now()))
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1)
+console.log(newDate.getDay())
+
+newDate.toLocaleString('default',{
+    
+})
