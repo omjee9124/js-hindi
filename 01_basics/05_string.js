b593f2a6874562105b1a0bb3d3bdca646e058ad8
OM JI DUBEY <omjeed0666@gmail.com>
2025-05-30 11:49:28 +0000

    Math and num and String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,30 @@
+// const name = "Om Ji"
+// const repoCount = 50
+// console.log(`My name is ${name} and my repo Count is ${repoCount}`)
+
+// const gameName = new String('Omji@123')
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5,8)
+// console.log(anotherString);
+
+
+// const oneAnotherString = "      Dubey    "
+// console.log(oneAnotherString);
+// console.log(oneAnotherString.trim());
+
+// const url = 'https:/omji.com/dubey%20jiom'
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('moc'))
+
+const splitexample = 'The quick brown fox jumps over the lazy dog.'
+console.log(splitexample.split(" ",5))
+const words = splitexample.split(" ")
+console.log(words[6])
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2));
+
+const otherNumber = 143.1476
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+// ++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.3));
+// console.log(Math.min(2,8,5,4,1))
+// console.log(Math.max(2,8,5,4,1))
+
+
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor((Math.random()*10)+1))
+
+const min = 1
+const max = 6
+
+//Very important line#########################
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+//############################################
